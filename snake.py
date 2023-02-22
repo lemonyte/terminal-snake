a0943174504821469e22ce5e23ddf9883df2bb26
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-02-22 13:02:30 -0500

    Cleanup and fix the game so it works again

--- snake.py
+++ snake.py
@@ -1,12 +1,20 @@
-import time
 import json
-from random import choice
+import time
 from dataclasses import dataclass
-from pyco import terminal, cursor
-from pyco.color import Fore, Back, RESET
+from enum import Enum
+from random import choice
+
+from pyco import cursor, terminal
+from pyco.color import RESET, Back, Fore
+from pyco.constants import ESC
 from pyco.utils import getch, kbhit
 
-ESC = '\x1b'
+
+class Direction(Enum):
+    UP = 'up'
+    DOWN = 'down'
+    LEFT = 'left'
+    RIGHT = 'right'
 
 
 @dataclass
@@ -33,14 +41,12 @@ class Dimensions(XYPair):
 
 
 class Field:
-    def __init__(self, game, config: dict):
+    def __init__(self, game: "Game", config: dict):
         self.game = game
         self.config = config
         self.snake = Snake(game, self, config)
-        self.scaling = self.config['field']['scaling']
         self.size = Dimensions(self.config['field']['width'], self.config['field']['height'])
-        self.real_size = Dimensions(self.size.x * self.scaling * 2, self.size.y * self.scaling)
-        self.enable_border = self.config['field']['enable_border']
+        self.real_size = Dimensions(self.size.x * 2, self.size.y)
         self.snake_coords: list[Point] = []
         self.field = [[{} for y in range(self.size.y)] for x in range(self.size.x)]
         self.create_food()
@@ -53,17 +59,7 @@ class Field:
     def clear(self):
         for y in range(self.size.y):
             for x in range(self.size.x):
-                if x == self.food_pos.x and y == self.food_pos.y:
-                    continue
-                else:
-                    self.field[x][y] = self.config['field']['empty']
-                if self.enable_border:
-                    if (x == 0 and y == 0) or (x == 0 and y == self.size.y - 1) or (x == self.size.x - 1 and y == 0) or (x == self.size.x - 1 and y == self.size.y - 1):
-                        self.field[x][y] = self.config['field']['border']['corner']
-                    elif x == 0 or x == self.size.x - 1:
-                        self.field[x][y] = self.config['field']['border']['vertical']
-                    elif y == 0 or y == self.size.y - 1:
-                        self.field[x][y] = self.config['field']['border']['horizontal']
+                self.field[x][y] = self.config['field']['empty']
 
     def update(self):
         self.clear()
@@ -73,12 +69,22 @@ class Field:
         self.field[head.x][head.y] = self.config['snake']['head']
         tail = self.snake_coords[0]
         self.field[tail.x][tail.y] = self.config['snake']['tail']
+        self.field[self.food_pos.x][self.food_pos.y] = self.config['field']['food']
+        rows = []
         for y in range(self.size.y):
             row = ''
             for x in range(self.size.x):
-                row += (getattr(Fore, self.field[x][y]['fore_color']) + getattr(Back, self.field[x][y]['back_color']) + self.field[x][y]['char'] + RESET) * 2 * self.scaling
-            for i in range(1, self.scaling + 1):
-                self.game.add_string((0, (y * self.scaling) + i), row)
+                row += (
+                    (
+                        getattr(Fore, self.field[x][y]['fore_color'].upper())
+                        + getattr(Back, self.field[x][y]['back_color'].upper())
+                        + self.field[x][y]['char']
+                        + RESET
+                    )
+                    * 2
+                )
+            rows.append(row)
+        self.game.add_string((0, 0), '\n'.join(rows))
         terminal.set_window_title(f"Terminal Snake Game - Score: {self.snake.length}")
 
     def create_food(self):
@@ -88,35 +94,47 @@ class Field:
             point = Point(choice(xr), choice(yr))
             if point not in self.snake_coords:
                 self.food_pos = point
-                self.field[self.food_pos.x][self.food_pos.y] = self.config['field']['food']
                 return
             xr.remove(point.x)
             yr.remove(point.y)
 
 
 class Snake:
-    def __init__(self, game, field: Field, config: dict):
+    def __init__(self, game: "Game", field: Field, config: dict):
         self.game = game
         self.field = field
         self.config = config['snake']
-        self.pos = Point(self.config['pos']['x'], self.config['pos']['y'])
-        self.length = self.config['length']
-        self.direction = self.config['direction'].upper()
+        self.coords: list[Point] = []
+        self.length = self.config['start_length']
+        self.direction = Direction(self.config['start_direction'])
         self.speed = 1 / self.config['speed']
         self.is_alive = True
         self.last_move_time = -self.speed
         self.last_direction_time = 0
-        if self.direction == 'R' or self.direction == 'L':
-            self.coords = [Point(self.pos.x + i if self.direction == 'R' else -i, self.pos.y) for i in range(self.length)]
-        elif self.direction == 'U' or self.direction == 'D':
-            self.coords = [Point(self.pos.x, self.pos.y + i if self.direction == 'D' else -i) for i in range(self.length)]
 
-    def change_direction(self, direction: str):
-        if direction == 'R' and self.direction == 'L': return
-        if direction == 'L' and self.direction == 'R': return
-        if direction == 'U' and self.direction == 'D': return
-        if direction == 'D' and self.direction == 'U': return
-        if self.last_direction_time > self.last_move_time: return
+        start_pos = Point(self.config['start_pos']['x'], self.config['start_pos']['y'])
+        if self.direction in (Direction.LEFT, Direction.RIGHT):
+            self.coords = [
+                Point(start_pos.x + i if self.direction == Direction.RIGHT else -i, start_pos.y)
+                for i in range(self.length)
+            ]
+        elif self.direction in (Direction.UP, Direction.DOWN):
+            self.coords = [
+                Point(start_pos.x, start_pos.y + i if self.direction == Direction.DOWN else -i)
+                for i in range(self.length)
+            ]
+
+    def change_direction(self, direction: Direction):
+        if direction == Direction.RIGHT and self.direction == Direction.LEFT:
+            return
+        if direction == Direction.LEFT and self.direction == Direction.RIGHT:
+            return
+        if direction == Direction.UP and self.direction == Direction.DOWN:
+            return
+        if direction == Direction.DOWN and self.direction == Direction.UP:
+            return
+        if self.last_direction_time > self.last_move_time:
+            return
         self.last_direction_time = time.perf_counter()
         self.direction = direction
 
@@ -155,13 +173,13 @@ class Snake:
     def move(self):
         if time.perf_counter() - self.last_move_time >= self.speed:
             head = self.coords[-1].copy()
-            if self.direction == 'R':
+            if self.direction == Direction.RIGHT:
                 head.x += 1
-            elif self.direction == 'L':
+            elif self.direction == Direction.LEFT:
                 head.x -= 1
-            elif self.direction == 'U':
+            elif self.direction == Direction.UP:
                 head.y -= 1
-            elif self.direction == 'D':
+            elif self.direction == Direction.DOWN:
                 head.y += 1
             body = self.coords[:-1]
             self.is_alive = head not in body
@@ -169,12 +187,16 @@ class Snake:
             self.coords.pop(0)
             self.field.snake_coords = self.coords
             if not self.is_alive:
-                self.game.end(Fore.BRIGHT_RED + "GAME OVER", Fore.WHITE + "PRESS 'ESC' TO EXIT OR ANY OTHER KEY TO PLAY AGAIN")
+                self.game.end(
+                    Fore.BRIGHT_RED + "GAME OVER", Fore.WHITE + "PRESS 'ESC' TO EXIT OR ANY OTHER KEY TO PLAY AGAIN"
+                )
             if self.field.food_pos in self.coords:
                 self.grow()
                 self.field.create_food()
             if self.length >= self.field.size.x * self.field.size.y:
-                self.game.end(Fore.BRIGHT_GREEN + "YOU WIN", Fore.WHITE + "PRESS 'ESC' TO EXIT OR ANY OTHER KEY TO PLAY AGAIN")
+                self.game.end(
+                    Fore.BRIGHT_GREEN + "YOU WIN", Fore.WHITE + "PRESS 'ESC' TO EXIT OR ANY OTHER KEY TO PLAY AGAIN"
+                )
             self.last_move_time = time.perf_counter()
 
 
@@ -184,30 +206,33 @@ class Game:
             self.config = json.load(config_file)
         self.keybinds = self.config['keybinds']
         self.running = True
+        self.field = Field(self, self.config)
 
     def play(self):
         self.running = True
         while self.running:
-            self.field = Field(self, self.config)
             while self.field.snake.is_alive and self.running:
                 key = self.get_key()
-                if key is not None:
-                    key = key.upper()
-                    # key = self.keybinds.get(key, key)
-                    if key in self.keybinds.values():
-                        if key == self.keybinds.get('exit'):
+                if key:
+                    key = key.lower()
+                    if key in self.keybinds:
+                        action = self.keybinds.get(key)
+                        if action == 'exit':
                             self.exit()
-                        elif key == self.keybinds.get('pause'):
+                        elif action == 'pause':
                             getch()
                         else:
-                            self.field.snake.change_direction(key)
+                            self.field.snake.change_direction(Direction(action))
                 self.field.snake.move()
                 self.field.update()
 
     def end(self, *messages: str):
         # terminal.bell()
         for i, message in enumerate(messages):
-            self.add_string(((self.field.real_size.x - len(message)) // 2, ((self.field.real_size.y - len(messages)) // 2) + i), message + RESET)
+            self.add_string(
+                ((self.field.real_size.x - len(message)) // 2, ((self.field.real_size.y - len(messages)) // 2) + i),
+                message + RESET,
+            )
         key = getch().upper()
         if key == '\x1b':
             self.exit()
@@ -232,4 +257,7 @@ class Game:
 
 if __name__ == '__main__':
     game = Game()
-    game.play()
+    try:
+        game.play()
+    except KeyboardInterrupt:
+        raise SystemExit
