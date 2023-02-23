6e908d185aa1b2c9de7ff0c61e79508fe518656b
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-02-22 23:15:07 -0500

    Add default values to config.py for use without config file

--- config.py
+++ config.py
@@ -4,31 +4,34 @@ from dataclasses import dataclass
 
 @dataclass
 class _ColorsConfig:
-    empty: str
-    food: str
-    snake: str
+    empty: str = 'black'
+    food: str = 'bright_red'
+    snake: str = 'bright_green'
 
 
 @dataclass
 class _Config:
-    snake_speed: float
-    loop: bool
-    colors: _ColorsConfig
+    snake_speed: int = 50
+    loop: bool = True
+    colors: _ColorsConfig = _ColorsConfig()
 
 
 def load_config(config_path: str = 'config.json') -> _Config:
-    with open(config_path, 'r') as file:
-        config_data = json.load(file)
+    try:
+        with open(config_path, 'r') as file:
+            config_data = json.load(file)
 
-    return _Config(
-        snake_speed=config_data['snake_speed'],
-        loop=config_data['loop'],
-        colors=_ColorsConfig(
-            empty=config_data['colors']['empty'],
-            food=config_data['colors']['food'],
-            snake=config_data['colors']['snake'],
-        ),
-    )
+        return _Config(
+            snake_speed=config_data['snake_speed'],
+            loop=config_data['loop'],
+            colors=_ColorsConfig(
+                empty=config_data['colors']['empty'],
+                food=config_data['colors']['food'],
+                snake=config_data['colors']['snake'],
+            ),
+        )
+    except (FileNotFoundError, KeyError):
+        return _Config()
 
 
 config = load_config()
